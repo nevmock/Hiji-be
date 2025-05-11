9c377885645e014c9f0ffd0efdadcb76ec900794
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2025-05-12 00:02:22 +0700

    set limit for each level

--- src/domains/web-builder/bussiness/bussiness-controller.js
+++ src/domains/web-builder/bussiness/bussiness-controller.js
@@ -2,26 +2,19 @@ import mongoose from "mongoose";
 import BaseError from "../../../base_classes/base-error.js";
 import { createdResponse, successResponse } from "../../../utils/response.js";
 import BussinessService from "./bussiness-service.js";
+import Bussiness from "../../../models/bussiness.js";
 
 class BussinessController {
     async index(req, res) {
-        const { user_id } = req.app.locals.user;
+        const { user } = req;
 
-        if (!user_id) {
-            throw Error("User not found");
-        }
-
-        const { bussiness, total } = await BussinessService.get(user_id);
+        const { bussiness, total } = await BussinessService.get(user.id);
 
         return successResponse(res, bussiness, total);
     }
 
     async show(req, res) {
-        const { user_id } = req.app.locals.user;
-
-        if (!user_id) {
-            throw Error("User not found");
-        }
+        const { user } = req;
 
         const { id } = req.params;
 
@@ -33,7 +26,7 @@ class BussinessController {
             throw BaseError.badRequest("Invalid user id");
         }
 
-        const bussiness = await BussinessService.getById(id, user_id);
+        const bussiness = await BussinessService.getById(id, user.id);
 
         if (!bussiness) {
             throw BaseError.notFound("Bussiness not found");
@@ -43,15 +36,25 @@ class BussinessController {
     }
 
     async create(req, res) {
-        const { user_id } = req.app.locals.user;
+        const { user } = req;
 
-        if (!user_id) {
-            throw Error("User not found");
+        const countBussiness = await Bussiness.countDocuments({ user_id: user.id })
+
+        let maxBussiness = 1;
+        
+        if (user.level === 2) {
+            maxBussiness = 3;
+        } else if (user.level === 3) {
+            maxBussiness = 20;
+        }
+
+        if (countBussiness >= maxBussiness) {
+            throw BaseError.forbidden(`Maximum Bussiness reached, upgrade your plan to create more Bussiness`);
         }
 
         const { bussiness_name, phone_number, email, location_id, address } = req.body;
         
-        const bussiness = await BussinessService.create({user_id, bussiness_name, phone_number, email, location_id, address });
+        const bussiness = await BussinessService.create({user_id: user.id, bussiness_name, phone_number, email, location_id, address });
 
         if (!bussiness) {
             throw Error("Failed to create bussiness");
@@ -61,11 +64,7 @@ class BussinessController {
     }
 
     async update(req, res) {
-        const { user_id } = req.app.locals.user;
-
-        if (!user_id) {
-            throw Error("User not found");
-        }
+        const { user } = req;
 
         const { id } = req.params;
 
@@ -75,7 +74,7 @@ class BussinessController {
 
         const data = req.body;
 
-        const bussiness = await BussinessService.update(user_id, id, data);
+        const bussiness = await BussinessService.update(user.id, id, data);
 
         if (!bussiness) {
             throw Error("Failed to update bussiness");
@@ -85,11 +84,7 @@ class BussinessController {
     }
 
     async delete(req, res) {
-        const { user_id } = req.app.locals.user;
-
-        if (!user_id) {
-            throw Error("User not found");
-        }
+        const { user } = req;
 
         const { id } = req.params;
 
@@ -97,7 +92,7 @@ class BussinessController {
             throw BaseError.badRequest("Id is required");
         }
 
-        const bussiness = await BussinessService.delete(user_id, id);
+        const bussiness = await BussinessService.delete(user.id, id);
 
         if (!bussiness) {
             throw Error("Failed to delete bussiness");
